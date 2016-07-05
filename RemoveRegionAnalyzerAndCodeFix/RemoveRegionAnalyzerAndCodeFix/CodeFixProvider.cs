a0ac569147e0a0d65d6afdec7d4e8dfaeca7cff0
filipw <wojcieszyn@gmail.com>
2016-07-05 22:26:18 +0200

    added first version

---
+++ RemoveRegionAnalyzerAndCodeFix/RemoveRegionAnalyzerAndCodeFix/CodeFixProvider.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace RemoveRegionAnalyzerAndCodeFix
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(RemoveRegionCodeFixProvider)), Shared]
+    public class RemoveRegionCodeFixProvider : CodeFixProvider
+    {
+        private const string title = "Get rid of the damn region";
+
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(RemoveRegionAnalyzer.DiagnosticId); }
+        }
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            var node = root.FindNode(diagnosticSpan, findInsideTrivia: true, getInnermostNodeForTie: true);
+            DirectiveTriviaSyntax region = node as RegionDirectiveTriviaSyntax;
+
+            if (region == null)
+            {
+                region = node as EndRegionDirectiveTriviaSyntax;
+            }
+
+            if (region != null)
+            {
+                context.RegisterCodeFix(
+                    CodeAction.Create(
+                        title,
+                        async c =>
+                        {
+                            var oldRoot = await context.Document.GetSyntaxRootAsync(c);
+                            var newRoot = oldRoot.ReplaceNodes(region.GetRelatedDirectives(), (trivia, syntaxTrivia) => SyntaxFactory.SkippedTokensTrivia());
+                            var newDocument = context.Document.WithSyntaxRoot(newRoot.NormalizeWhitespace());
+                            return newDocument;
+                        }),
+                    diagnostic);
+            }
+        }
+    }
+}
---
+++ RemoveRegionAnalyzerAndCodeFix/RemoveRegionAnalyzerAndCodeFix/DiagnosticAnalyzer.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace RemoveRegionAnalyzerAndCodeFix
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class RemoveRegionAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "RemoveRegionAnalyzer";
+
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Naming";
+
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeNode, ImmutableArray.Create(SyntaxKind.RegionDirectiveTrivia, SyntaxKind.EndRegionDirectiveTrivia));
+        }
+
+        private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var diagnostic = Diagnostic.Create(Rule, context.Node.GetLocation(), context.Node.GetText().ToString().Replace(Environment.NewLine, string.Empty));
+            context.ReportDiagnostic(diagnostic);
+        }
+    }
+}
